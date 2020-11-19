85de96c90a0e55d9c9b49871bca276910b3db110
winnermann <zotov.k1983@mail.ru>
2020-11-19 21:25:24 +0300

    Добавлены проверки ячеек таблицы ResultSet и RowSet

--- src/main/java/at/jdbc/JDBCExample3.java
+++ src/main/java/at/jdbc/JDBCExample3.java
@@ -1,13 +1,14 @@
 package at.jdbc;
 
 import at.common.ConfigJDBC3;
+import com.sun.rowset.JdbcRowSetImpl;
 import io.qameta.allure.Step;
 
+import javax.sql.rowset.JdbcRowSet;
 import java.sql.*;
 
 import static io.restassured.RestAssured.*;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 public class JDBCExample3 {
 
@@ -43,6 +44,60 @@ public class JDBCExample3 {
         System.out.println(resultSet.findColumn("color"));
         System.out.println(resultSet.findColumn("type"));
         System.out.println(resultSet.findColumn("price"));
+
+
+
+        /**
+         * Обработаем результат
+         * Проверка результатов из таблицы
+         * способом ResultSet
+         */
+
+        //устанавливает курсор в первую строку
+        resultSet.next();
+        //получает значение из ячейки на пересечении столбца code и строки выделенной методом next()
+        int code = resultSet.getInt("code");
+        assertEquals(1, code);
+        System.out.println(code);
+
+        int model = resultSet.getInt("model");
+        assertEquals(1276, model);
+        System.out.println(model);
+
+        String color = resultSet.getString("color");
+        assertEquals("n", color);
+        System.out.println(color);
+
+        String type = resultSet.getString("type");
+        assertEquals("Laser", type);
+        System.out.println(type);
+
+        int price = resultSet.getInt("price");
+        assertEquals(400, price);
+        System.out.println(price);
+
+
+
+        /**
+         * Обработаем результат
+         * Проверка результатов из таблицы
+         * способом RowSet
+         */
+
+        JdbcRowSet jdbcRs = new JdbcRowSetImpl(con);
+        jdbcRs.setCommand("select * from product");
+        jdbcRs.execute();
+        jdbcRs.next();
+
+        String maker = jdbcRs.getString("maker");
+        System.out.println(maker);
+
+        model = jdbcRs.getInt("model");
+        System.out.println(model);
+
+        type = jdbcRs.getString("type");
+        System.out.println(type);
+
 
         //Закрыть подключение
         con.close();
