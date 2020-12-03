a9628095825dbcaefd27b177b049b67713cbdb6a
winnermann <zotov.k1983@mail.ru>
2020-12-03 21:10:12 +0300

    Добавлена проверка Selenide Login

---
+++ src/test/java/at/tests/AutomationpracticeUITest.java
@@ -0,0 +1,18 @@
+package at.tests;
+
+import at.web.AutomationpracticeUI;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.testng.annotations.Test;
+
+public class AutomationpracticeUITest {
+    @Test
+    @Description(value = "UI")
+    @Epic("Функциональные тесты")
+    @Severity(value = SeverityLevel.NORMAL)
+    public void testCases(){
+        AutomationpracticeUI.login();
+    }
+}
