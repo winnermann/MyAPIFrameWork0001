3ede2fba3c51cd34d10f7a81dea892436458b67a
winnermann <zotov.k1983@mail.ru>
2021-01-30 20:46:43 +0300

    DownloadFileUsingRestAssured

---
+++ src/test/java/at/tests/DownloadFileUsingRestAssuredTest.java
@@ -0,0 +1,22 @@
+package at.tests;
+
+import at.api.DownloadFileUsingRestAssured;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class DownloadFileUsingRestAssuredTest {
+
+    @Test
+    @Description(value = "API")
+    @Epic("Функциональные тесты")
+    @Severity(value = SeverityLevel.NORMAL)
+
+    public void testCases() throws IOException {
+        DownloadFileUsingRestAssured.canDownloadFilesWithRestAssured();
+    }
+}
